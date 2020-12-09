45bdc7098dcc98cc769d23d866d099e956c43df6
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-12-10 00:12:44 +0530

    add: implement code for name validation on input field

--- src/script.js
+++ src/script.js
@@ -4,3 +4,10 @@ output.textContent = salary.value;
 salary.addEventListener("input", function () {
   output.textContent = salary.value;
 });
+const text = document.querySelector("#name");
+const textError = document.querySelector(".text-error");
+text.addEventListener("input", function () {
+  let namRegex = new RegExp(/^[A-Z]{1}[a-z ]{2,}$/);
+  if (namRegex.test(text.value)) textError.textContent = "";
+  else textError.textContent = "Name is incorrect";
+});
