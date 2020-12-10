0aabf0727a52d0674698b41227829a49045066c6
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-12-11 01:18:37 +0530

    add: code to store data on local storage

--- src/script.js
+++ src/script.js
@@ -101,11 +101,22 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const save = () => {
     try {
       let employeePayrollData = creaateEmployeePayroll();
-    //   createAndUpdateStorage(employeePayrollData);
+      createAndUpdateStorage(employeePayrollData);
     } catch (e) {
       return;
     }
   };
+
+  function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if (employeePayrollList != undefined) {
+      employeePayrollList.push(employeePayrollData);
+    } else {
+      employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem( "EmployeePayrollList", JSON.stringify(employeePayrollList));
+  }
   
 const creaateEmployeePayroll = () => {
     let employeePayrollData = new EmployeePayrollData();
